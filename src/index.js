fac9bcec50b6464f753eb8c5995570622e7726be
Diana-Sh1 <dianaa15052015@gmail.com>
2023-03-09 15:57:24 +0300

    store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
-import state from './redux/state';
-import {rerenderEntireTree} from "./render";
+import state, {subscribe} from './redux/state';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
+
+import {addPost, updateNewPostText} from './redux/state';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+let rerenderEntireTree = (state)=> {
+
+    root.render(
+        <React.StrictMode>
+            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+        </React.StrictMode>
+    );
+}
+
+subscribe(rerenderEntireTree);
 
 rerenderEntireTree(state);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,5 @@
-import {rerenderEntireTree} from "../render";
+let rerenderEntireTree = () => {
+}
 
 let state = {
     profilePage: {
@@ -58,4 +59,58 @@ export let updateNewPostText = (newText)=> {
     rerenderEntireTree(state);
 
 }
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer;
+}
 export default state;
+
+let store = {
+    _subscriber() {},
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hi, how are you?'},
+                {id: 2, message: 'I\'s my first react app'}
+            ],
+            newPostText: ''
+        },
+        dialogsPage: {
+            messages: [
+                {id: 1, message: 'Hi', person: 'Grigoriy', src: 'logo_dialog.png'},
+                {
+                    id: 2,
+                    message: 'How are you?Lorem Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab atque dicta fuga hic labore laudantium libero nostrum, nulla, om',
+                    person: 'Me',
+                    src: 'logo_dialog2.png'
+                },
+                {id: 1, message: 'Hi', person: 'Grigoriy', src: 'logo_dialog.png'},
+                {
+                    id: 2,
+                    message: 'How are you?Lorem Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab atque dicta fuga hic labore laudantium libero nostrum, nulla, om',
+                    person: 'Me',
+                    src: 'logo_dialog2.png'
+                },
+            ],
+            dialogs: [
+                {id: 1, name: 'Grigoriy'},
+                {id: 2, name: 'Anna'},
+                {id: 3, name: 'Olya'},
+                {id: 4, name: 'Alisa'},
+                {id: 5, name: 'Artem'},
+                {id: 6, name: 'Petya'}
+            ]
+        },
+        sidebar: [
+            {id: 1, person: 'Petya', src: 'friend1.png'},
+            {id: 2, person: 'Anna', src: 'friend2.png'},
+            {id: 3, person: 'Grigoriy', src: 'logo_dialog.png'}
+        ]
+    },
+    getState() {
+        return this._state;
+    },
+    subscribe(observer) {
+        this._subscriber = observer;
+    },
+
+}
