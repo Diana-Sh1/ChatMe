8a62355053c10c1554855546522dc8797be2560d
Diana-Sh1 <dianaa15052015@gmail.com>
2023-03-13 14:06:59 +0300

    textare in dialogs

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -3,14 +3,7 @@ import {sendMessageCreator, updateNewMessageBodyCreator} from "../../../redux/st
 
 
 const Message = (props) => {
-    let newMessageBody = props.state.newMessageBody;
-    let onSendMessageClick = () => {
-        props.store.dispatch(sendMessageCreator());
-    }
-    let onNewMessageChange = (e) => {
-        let body = e.target.value;
-        props.store.dispatch(updateNewMessageBodyCreator(body));
-    }
+
     return (
         <div className={s.person_message}>
             <div className={s.logo_name}>
@@ -20,12 +13,9 @@ const Message = (props) => {
                 <span>{props.person}</span>
             </div>
             <div className={s.message}>{props.message}</div>
-            <div className={s.enter_text}>
-                <textarea value={newMessageBody}
-                          onChange={onNewMessageChange}></textarea>
-                <button onClick={onSendMessageClick}><img src="arrow3.svg" alt=""/></button>
-            </div>
+
         </div>
+
     )
 }
 
