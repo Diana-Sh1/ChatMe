b89c4cf987bd69c41dc67303799bc8307a7ad83f
Diana-Sh1 <dianaa15052015@gmail.com>
2023-03-19 16:51:34 +0300

    copy in reducers

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import {Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-let rerenderEntireTree = ()=> {
 
     root.render(
         <React.StrictMode>
@@ -18,11 +17,8 @@ let rerenderEntireTree = ()=> {
             </Provider>
         </React.StrictMode>
     );
-}
 
-rerenderEntireTree();
 
-store.subscribe(()=> {
-    rerenderEntireTree();
-});
+
+
 
