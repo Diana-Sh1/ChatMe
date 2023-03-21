72337f3fe8a3afdf9d3a82e174d2799c91f2ec26
Diana-Sh1 <dianaa15052015@gmail.com>
2023-03-21 10:35:18 +0300

    copy in reducers

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -29,21 +29,22 @@ let initialState = {
     newMessageBody: ""
 }
 const dialogsReducer = (state = initialState, action) => {
-    let stateCopy;
+
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_BODY:
-            stateCopy = {...state, newMessageBody: action.body};
-            return stateCopy;
+           return {...state, newMessageBody: action.body};
         case SEND_MESSAGE:
             let body = state.newMessageBody;
-            stateCopy = {...state, newMessageBody: '', messages: [...state.messages]};
-            stateCopy.messages.push({id: 6, message: body, person: 'Grigoriy', src: 'logo_dialog.png'})
-            return stateCopy;
+            return {
+                ...state,
+                newMessageBody: '',
+                messages: [...state.messages, {id: 6, message: body, person: 'Grigoriy', src: 'logo_dialog.png'}]
+            };
         default:
             return state;
     }
 
 }
-export const sendMessageCreator = () => ({ type: SEND_MESSAGE});
-export const updateNewMessageBodyCreator = (body) => ({ type: UPDATE_NEW_MESSAGE_BODY, body: body})
+export const sendMessageCreator = () => ({type: SEND_MESSAGE});
+export const updateNewMessageBodyCreator = (body) => ({type: UPDATE_NEW_MESSAGE_BODY, body: body})
 export default dialogsReducer;
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -9,23 +9,21 @@ let initialState = {
 }
 
 const profileReducer = (state = initialState,action) => {
-    let stateCopy;
     switch (action.type) {
         case ADD_POST: {
             let newPost = {
                 id: 5,
                 message: state.newPostText
             };
-            stateCopy = {...state};
-            stateCopy.posts = [...state.posts];
-            stateCopy.posts.push(newPost);
-            stateCopy.newPostText = '';
-            return stateCopy;
+            return {...state,
+                posts: [...state.posts, newPost],
+                newPostText: ''
+            };
         }
         case UPDATE_NEW_POST_TEXT: {
-            stateCopy = {...state};
-            stateCopy.newPostText = action.newText;
-            return stateCopy;
+           return {...state,
+                newPostText: action.newText
+            };
     }
         default:
             return state;
