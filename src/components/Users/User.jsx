c9934ed49c51c8ea81c545aadabf6d2bcb03214e
Diana-Sh1 <dianaa15052015@gmail.com>
2023-04-30 19:24:37 +0300

    90 refactored Users

---
+++ src/components/Users/User.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import s from "./Users.module.css"
+import {NavLink} from "react-router-dom";
+import userDefaultPic from '../../assets/user_default.png'
+
+
+let User = ({user, followingInProgress, unfollow, follow}) => {
+    return <div className={s.inner}>
+        <div className={s.user_logo}>
+            <NavLink to={'/profile/' + user.id}>
+                <img src={user.photos.small != null ? user.photos.small : userDefaultPic} alt=""/>
+            </NavLink>
+
+        </div>
+        <div className={s.block_main}>
+            <div className={s.user_info}>{user.name}</div>
+            <div className={s.status_info}>{user.status}</div>
+        </div>
+        <div className={s.block_buttons}>
+            {user.followed
+                ? <button disabled={followingInProgress
+                    .some(id => id === user.id)} onClick={() => { unfollow(user.id)
+                }} className={s.subscribe_btn}>Unfollow</button>
+                : <button disabled={followingInProgress
+                    .some(id => id === user.id)} onClick={() => {follow(user.id)
+                }} className={s.subscribe_btn}>Follow</button>
+            }
+        </div>
+        </div>
+        }
+export default User;
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import s from "./Users.module.css"
-import {NavLink} from "react-router-dom";
-import userDefaultPic from '../../assets/user_default.png'
-
-import isDivisibleBy from "validator/es/lib/isDivisibleBy";
 import Paginator from "../common/Paginator/Paginator";
+import User from "./User";
 
 
 let Users = ({currentPage, onPageChanged, totalUsersCount, pageSize, ...props}) => {
@@ -12,36 +9,16 @@ let Users = ({currentPage, onPageChanged, totalUsersCount, pageSize, ...props}) 
     return <div className={s.content}>
         <h2 className={s.title}>List of Users</h2>
         <div className={s.pagination}>
-
-            <Paginator currentPage={currentPage} onPageChanged={onPageChanged} totalUsersCount={totalUsersCount}
+            <Paginator currentPage={currentPage}
+                       onPageChanged={onPageChanged}
+                       totalUsersCount={totalUsersCount}
                        pageSize={pageSize}/>
         </div>
-        {
-            props.users.map(u =>
-                <div className={s.inner}>
-                    <div className={s.user_logo}>
-                        <NavLink to={'/profile/' + u.id}>
-                            <img src={u.photos.small != null ? u.photos.small : userDefaultPic} alt=""/>
-                        </NavLink>
-
-                    </div>
-                    <div className={s.block_main}>
-                        <div className={s.user_info}>{u.name}</div>
-                        <div className={s.status_info}>{u.status}</div>
-                    </div>
-                    <div className={s.block_buttons}>
-                        {u.followed
-                            ? <button disabled={props.followingInProgress
-                                .some(id => id === u.id)} onClick={() => {
-                                props.unfollow(u.id)
-                            }} className={s.subscribe_btn}>Unfollow</button>
-                            : <button disabled={props.followingInProgress
-                                .some(id => id === u.id)} onClick={() => {
-                                props.follow(u.id)
-                            }} className={s.subscribe_btn}>Follow</button>}
-                    </div>
-
-                </div>)
+        { props.users.map(u => <User key={u.id} user={u}
+                                       followingInProgress={props.followingInProgress}
+                                       unfollow={props.unfollow}
+                                       follow={props.follow}
+            />)
         }
     </div>
 }
