a86f61b0fe82f74f14174cbc7181a8302595cbf6
Diana-Sh1 <dianaa15052015@gmail.com>
2023-05-08 15:55:12 +0300

    97 profile refactoring4

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,28 +2,35 @@ import s from './ProfileInfo.module.css'
 import Preloader from "../../common/Preloader/Preloader";
 import yes from "../../../assets/yes_pic.png"
 import no from "../../../assets/no_pic.png"
+import loadPhoto from "../../../assets/loadImage.svg"
 import userDefaultPic from "../../../assets/user_default2.png";
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 import {useState} from "react";
 import ProfileDataForm from "./ProfileDataForm";
 
 
-const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto, saveProfile, messages}) => {
+const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto, saveProfile, messages,}) => {
     let [editMode, setEditMode] = useState(false);
-    let [isHovering, setIsHovering] = useState(false);
 
     const Contact = ({contactTitle, contactValue}) => {
         return <div>{contactTitle} - <a href={contactValue}>{contactValue}</a></div>
     }
     const ProfileData = ({profile, isOwner, goToEditMode}) => {
         return <div className={s.card}>
-            {isOwner && <button onClick={goToEditMode}>Edit profile</button>}
             <h2>{profile.fullName}</h2>
+            <div className={s.edit_buttons}>
+                <div className={s.btn_profile}>{isOwner &&
+                    <button onClick={goToEditMode} className={s.edit_profile}>Edit profile</button>}</div>
+                <div className={s.edit_photo}>
+                    {isOwner &&
+                        <input type={"file"} onChange={onMainPhotoSelected} className={s.visible}/>}
+                </div>
+            </div>
             <div className={s.inner}>
                 <span className={s.title}>Contacts:</span>
                 {Object.keys(profile.contacts).map(key => {
-                return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]}/>
-            })}
+                    return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]}/>
+                })}
                 <span className={s.title}>
                         Looking for a job: {profile.lookingForAJob ?
                     <img alt='' className={s.job_img} src={yes}></img> :
@@ -53,14 +60,13 @@ const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto, savePro
     }
     return (
         <div className={s.content}>
-            <div className={s.image}
-                onMouseEnter={() => setIsHovering(true)}
-                onMouseLeave={() => setIsHovering(false)}>
-                <img alt='' src={profile.photos.large || userDefaultPic} ></img>
-                <span>{isOwner &&
-                    <input type={"file"} onChange={onMainPhotoSelected} className={isHovering ? s.visible : s.hidden}/>}
-                </span>
+            <div className={s.image_content}>
+                <div className={s.image}>
+                    <img alt='' src={profile.photos.large || userDefaultPic}></img>
+                </div>
+                {/*tut*/}
             </div>
+
             {editMode
                 ? <ProfileDataForm profile={profile} status={status} updateStatus={updateStatus}
                                    onSubmit={onSubmit} messages={messages}/>
