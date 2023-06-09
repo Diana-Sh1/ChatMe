a67be40b2248845f95604f3a0911cc730db9dbd0
Diana-Sh1 <dianaa15052015@gmail.com>
2023-06-09 11:39:51 +0300

    searchForm followed

--- src/components/Users/UsersSearchForm.tsx
+++ src/components/Users/UsersSearchForm.tsx
@@ -2,6 +2,7 @@ import {useForm} from "react-hook-form";
 import React, {FC} from "react";
 import {FilterType} from "../../redux/users-reducer";
 import s from "./Users.module.css"
+import Button from "../Button/Button";
 
 type PropsType = {
     onFilterChanged: (filter: FilterType)=> void
@@ -16,21 +17,17 @@ export const UsersSearchForm: FC<PropsType> = (props) => {
     const {register, formState: {errors}, handleSubmit} = useForm<FormValues>({
         mode: "onSubmit"
     });
-    return <div className={s.form}>
+    return <div className={s.form_wrapper}>
         <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register('term', {
-                maxLength: {
-                    value: 10,
-                    message: "Max length 10 symbols"
-                }
-            })}/>
-            <select {...register("friend")}>
+            <input className={s.input} {...register('term')}/>
+            <select className={s.select} {...register("friend")}>
                 <option value="null">All</option>
                 <option value="true">Followed</option>
                 <option value="false">Unfollowed</option>
             </select>
-            <span>{errors?.term && <p className={s.errors}>{errors.term?.message} </p>}</span>
-            <input type="submit" value="Find"></input>
+            {/*<span>{errors?.term && <p className={s.errors}>{errors.term?.message} </p>}</span>*/}
+            {/*<input className={s.btn} type="submit" value="Find"></input>*/}
+            <input type="submit" value="Find" className={s.btn}></input>
         </form>
     </div>
 
