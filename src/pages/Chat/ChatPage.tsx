ab0b0f7c0f789aa02c2ad5f824b951b5d3451cf1
Diana-Sh1 <dianaa15052015@gmail.com>
2023-06-14 14:50:55 +0300

    websocket

--- src/pages/Chat/ChatPage.tsx
+++ src/pages/Chat/ChatPage.tsx
@@ -1,9 +1,15 @@
-import {FC} from "react";
+import {FC, useEffect, useState} from "react";
 import pic from "../../assets/friend2.png"
 import s from "./ChatPage.module.css"
+import message from "../../components/Dialogs/Message/Message";
 
-const ws = new WebSocket()
-
+const ws = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
+export type ChatMessageType = {
+    message: string
+    photo: string
+    userId: number
+    userName: string
+}
 const ChatPage: FC = () => {
     return <div>
         <Chat/>
@@ -12,29 +18,30 @@ const ChatPage: FC = () => {
 
 
 const Chat: FC = () => {
+
     return <div>
         <Messages/>
         <AddMessageForm/>
     </div>
 }
 const Messages: FC = () => {
-    const messages = [1,2,3,4]
+ const [messages, setMessages] = useState<ChatMessageType[]>([])
+    useEffect(()=>{
+        ws.addEventListener('message', (e)=>{
+            setMessages(JSON.parse(e.data))
+        })
+    },[])
+
     return <div style={{height: "400px", overflowY: "auto"}}>
-        {messages.map ((m: any) => <Message/>)}
-        {messages.map ((m: any) => <Message/>)}
-        {messages.map ((m: any) => <Message/>)}
+        {messages.map ((m, index) => <Message key={index} message={m}/>)}
+
     </div>
 }
-const Message: FC = () => {
-    const message = {
-        author: 'Di',
-        text: 'Hello, friend',
-        src: pic
-    }
+const Message: FC<{message: ChatMessageType}> = ({message}) => {
     return <div>
-        <img  className={s.pic} src={message.src}/> <b>{message.author}</b>
+        <img  className={s.pic} src={message.photo}/> <b>{message.userName}</b>
         <br/>
-        {message.text}
+        {message.message}
         <hr/>
     </div>
 }
